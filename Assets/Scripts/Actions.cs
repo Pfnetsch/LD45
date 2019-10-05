4a8516e6c0b868ebbcd4fe5d16055b80671b599d
Patrick Seebacher <patrick.seebacher@gmail.com>
2019-10-05 20:42:18 +0200

    Sprites added

---
+++ Assets/Scripts/Actions.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Actions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DropDownGrassIndexChanged(int index)
+    {
+
+    }
+
+    public void DropDownBushIndexChanged(int index)
+    {
+
+    }
+
+    public void DropDownTreeIndexChanged(int index)
+    {
+        if (index == 0)
+        {
+            Vegetation treeZeroSprite = new TestTree();
+
+            treeZeroSprite.getTileForLevel(0);
+        }
+    }
+
+
+}
