9f17738a19829d1ad51cb2d98baa2c0da0c8498b
Patrick Seebacher <patrick.seebacher@gmail.com>
2019-10-05 13:56:42 +0200

    Tilemap Tests

---
+++ Assets/Scripts/GenerateMap.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GenerateMap : MonoBehaviour
+{
+    public Tile snowTile;
+    public Tile desertTile;
+    public Tile treeTile;
+    public Tile waterTile;
+
+    public Tilemap tileMap;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        int xCount = tileMap.size.x;
+        int yCount = tileMap.size.y;
+
+        for (int x = 0; x < xCount; x++)
+        {
+            for (int y = 0; y < yCount; y++)
+            {
+                Vector3Int pos = new Vector3Int(x, y, 0);
+
+                tileMap.SetTile(pos, treeTile);
+            }
+        }
+
+
+        //TileMap.SetTile()
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
