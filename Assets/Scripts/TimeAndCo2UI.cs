4e6b24f59c2b0364aa75a2fe7419ce1f84c6fa34
Patrick Seebacher <patrick.seebacher@gmail.com>
2019-10-07 00:20:55 +0200

    UI - Date added

--- Assets/Scripts/TimeAndCo2UI.cs
+++ Assets/Scripts/TimeAndCo2UI.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimeAndCo2UI : MonoBehaviour
 {
     public HexMap hexMap;
     public Transform co2GaugePointer;
+    public Transform dateText;
 
     // Start is called before the first frame update
     void Start()
@@ -17,5 +19,6 @@ public class TimeAndCo2UI : MonoBehaviour
     void Update()
     {
         co2GaugePointer.rotation = new Quaternion(0, 0, -(float)(hexMap.getCO2Level() * 2 - 1), 1);
+        dateText.GetComponent<Text>().text = hexMap.getDate().ToString("MMM yyyy");
     }
 }
