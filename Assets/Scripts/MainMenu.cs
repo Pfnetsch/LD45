96a151b1bfae2398afcb3a18a6573abf3b5133dd
Patrick Seebacher <patrick.seebacher@gmail.com>
2019-10-07 11:50:33 +0200

    MainMenu fixed

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,7 +7,6 @@ public class MainMenu : MonoBehaviour
 {
     public void playGame()
     {
-        // SceneManager.LoadScene(1);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
